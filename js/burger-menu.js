6fa509d6ce12d1ed3b41600a5910424cf873a80e
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-07-12 18:25:33 +0700

    restart

--- js/burger-menu.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#burger').addEventListener('click', function() {
-    document.querySelector('#nav-menu').classList.toggle('nav--active');
-  });
-});
