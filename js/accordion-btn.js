6d6c34dd00a8c9f90e3fd8dfa1b9a35e468f5c88
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-07-15 20:34:55 +0700

    added accordion section

---
+++ js/accordion-btn.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let acc = new Accordion('.accordion__list', {
+    duration: 700,
+    elementClass: 'accordion__item',
+    triggerClass: 'accordion__top',
+    panelClass: 'accordion__bottom',
+    showMultiple: false
+  })
+})
